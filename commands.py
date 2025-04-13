5cee443b7e3117925dde036b5943574bca69ede3
EgorSkab <7933556egor@gmail.com>
2025-04-13 18:40:46 +0300

    Updated commands.py get_connected_devices_by_class()

--- commands.py
+++ commands.py
@@ -1,8 +1,11 @@
 import subprocess
 
-def get_connected_devices_by_class(device_type):
+def get_connected_devices_by_class(device_type=""):
     try:
-        return subprocess.check_output(f"powershell.exe Get-PnpDevice -Class \"{device_type}\" -Status \"OK\" | Select-Object -Property Status, FriendlyName, InstanceID", text=True).strip()
+        if device_type == "":
+            return subprocess.check_output(f'powershell.exe Get-PnpDevice -Status \"OK\" | Select-Object -Property Class, FriendlyName, InstanceID', text=True, errors="replace").strip()
+        else:
+            return subprocess.check_output(f'powershell.exe Get-PnpDevice -Class \"{device_type}\" -Status \"OK\" | Select-Object -Property FriendlyName, InstanceID', text=True, errors="replace").strip()
     except subprocess.CalledProcessError as e:
         return f"Error retrieving {device_type} devices: {str(e)}"
 
